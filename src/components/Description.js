6a9e50cf54edeead8480a10a4bc8e09e1c1a4f99
dawn-boy <curiouslittleboy@pm.me>
2025-06-15 17:32:11 +0530

    divided into components

---
+++ src/components/Description.js
@@ -0,0 +1,10 @@
+export const Description = () => {
+    return (
+        <div className="description">
+            <h1 className="desc-title">The <span className="gradient-text">Arena</span> Talks!</h1>
+            <p className="desc-paragraph">
+                Step into the halls of cinema. Discover timeless classics, trending blockbusters, and hidden indie gems — all in one place.
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+export const Logo = () => {
+    return (
+        <h1 className="logo">
+            <span className="the">T</span>
+            <span className="the">he</span>
+            <span className="big-letter-col">C</span>
+            <span className="small-letters-col">olosseum</span>
+        </h1>
+    )
+}
